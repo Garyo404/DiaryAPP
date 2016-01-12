62c238f4f2b56cb8db79dcd96e1847ffc7dd1a8b
Garyo404 <garyo0404@gmail.com>
2016-01-12 16:59:41 +0800

    first commit

---
+++ XiaoJi小记/DiaryLabel.swift
@@ -0,0 +1,21 @@
+//
+//  DiaryLabel.swift
+//  XiaoJi小记
+//
+//  Created by Garyo on 16/1/12.
+//  Copyright © 2016年 ZJY. All rights reserved.
+//
+
+import UIKit
+
+class DiaryLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ XiaoJi小记/HomeYearCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomeYearCollectionViewCell.swift
+//  XiaoJi小记
+//
+//  Created by Garyo on 16/1/12.
+//  Copyright © 2016年 ZJY. All rights reserved.
+//
+
+import UIKit
+
+class HomeYearCollectionViewCell: UICollectionViewCell {
+    
+}
