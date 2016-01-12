efa49445fcb7b87164cad52110b5a6dd7a796d15
chenziang <chenziangsh@gmail.com>
2016-01-12 10:19:41 +0800

    Initial Commit

---
+++ XiaoJi小记/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XiaoJi小记
+//
+//  Created by Garyo on 16/1/12.
+//  Copyright © 2016年 ZJY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
